0a9fdb12d67e09c95ace49473dd1bba9ddacf8ee
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-04-18 14:09:13 +1000

    commented code

--- modules/u3_organisationAndReources/naming/commentMe.py
+++ modules/u3_organisationAndReources/naming/commentMe.py
@@ -1,10 +1,21 @@
+"""
+Prog:   rectCalc.py
+Name:   Tom Poletti
+Date:   2018/04/18
+Desc:   Calculates the area and perimeter of a rectangle.
+"""
+
+#print welcome message
 print('Welcome to the Circle Calculator!')
 
+#takes user input
 r = input('Enter a radius: ')
 r = int(r)
 
+#calculates and prints area
 area = 3.14 * r * r
 print('The area is', area)
 
+#calculates and prints perimeter
 perimeter = 3.14 * r * 2
 print('The perimeter is', perimeter)
