cc6601ea11d641695d35d4caf5d0e5870e7dbbc5
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-04-18 14:14:11 +1000

    Finished the code

--- modules/u3_organisationAndReources/naming/completeMe.py
+++ modules/u3_organisationAndReources/naming/completeMe.py
@@ -10,19 +10,20 @@ print('Welcome to the Rectangle Calculator!')
 
 # Use input to get the rectangle's length and width (2 lines).
 # - Remember to provide a prompt message for each input.
-
-
+length = input('Enter length: ')
+width = input('Enter width: ')
 
 # Convert length and width to integers.
 length = int(length)
 width = int(width)
 
 # Calculate the area (1 line: length times width).
-
+area=(length*width)
 
 # Display the area.
 print('The area is', area)
 
 # Calculate and display the perimeter (2 lines: P = 2*length + 2*width).
-
+P=(2*length + 2*width)
+print('The perimeter is', P)
 
