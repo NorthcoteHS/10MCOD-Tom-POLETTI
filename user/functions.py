2fe157f5530ca6273629cb71a0d93e6411ebf73a
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-05-01 10:36:38 +1000

    Added code to file

--- user/functions.py
+++ user/functions.py
@@ -1,4 +1,17 @@
+def hooray():
+    print('Hooray!')
+
+hooray()
+
+
 def power(x, y):
     return(x ** y)
 
 print('10 to the power of 5 is, ', power(10, 5))
+
+
+def happyBirthday():
+    name=input('What is you name? ')
+    print('Happy Birthday', name+'!')
+
+happyBirthday()
