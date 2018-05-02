1ae8ecd041e9b3f92e90d6b569a7ca5241cf6b99
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-05-02 14:43:02 +1000

    new edit

---
+++ modules/u4_programming/Lists.py
@@ -0,0 +1,9 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letter1 = alphabet[0]
+print(letter1)
+
+print(alphabet[5])
+amount=len(alphabet)
+print(amount)
+
+print(alphabet[19]+alphabet[14]+alphabet[12])
