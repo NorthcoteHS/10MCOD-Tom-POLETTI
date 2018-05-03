d647f988399edb1c3d38158d70743084843472d2
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-05-03 12:28:19 +1000

    work

--- modules/u4_programming/squareNumbers.py
+++ modules/u4_programming/squareNumbers.py
@@ -5,7 +5,7 @@ Date:   2018/05/03
 Desc:   This program will use a for loop for the numbers 1 - 10. Each number should be squared and the result added to a new list called squares. Print the list at the end of the file.
 """
 
-num = 1
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 squares = []
 
 for num in range (11):
