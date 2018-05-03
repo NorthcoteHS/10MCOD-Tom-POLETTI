8d397b5894943b9ce4bcb664a6fef4992a70faa8
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-05-03 12:26:47 +1000

    more work 3/5/18

--- modules/u4_programming/squareNumbers.py
+++ modules/u4_programming/squareNumbers.py
@@ -1,7 +1,15 @@
 """
-Prog:   goodDog.py
+Prog:   squareNumbers.py
 Name:   Tom Poletti
 Date:   2018/05/03
-Desc:   Calls each dog a good boy
+Desc:   This program will use a for loop for the numbers 1 - 10. Each number should be squared and the result added to a new list called squares. Print the list at the end of the file.
 """
 
+num = 1
+squares = []
+
+for num in range (11):
+    squares.append(num**2)
+    num = num+1
+
+print(squares)
