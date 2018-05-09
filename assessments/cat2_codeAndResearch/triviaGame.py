22f1140b02c106a406b8bc07737eea63cb8eb9af
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-05-09 14:22:57 +1000

    moreWork

--- assessments/cat2_codeAndResearch/triviaGame.py
+++ assessments/cat2_codeAndResearch/triviaGame.py
@@ -5,12 +5,24 @@ Date:   2018/05/09
 Desc:   Maths Trivia Game
 """
 
-qs = ['43+109=', '4*29=', '1836/12=', '259-74=', '3**5=']
+qs = ['43 plus 109 equals ', '4 times 29 equals ', '1836 divided by 12 equals ', '259 minus 74 equals ', '3 to the power of 5 equals ']
 ans = ['152', '116', '153', '185', '243']
+
+count=int(0)
 
 for i,item in enumerate(qs):
     inp=input(item)
     if inp == (ans[i]):
         print('Correct!')
+        count=(count+1)
     else:
         print('Incorrect!')
+
+mess1 = ['Unlucky, ', 'Not bad, ', 'Perfect, ']
+mess2 = ('you scored')
+if count == 0, 1:
+    print(mess1[0]+mess2, count)
+elif count == 2, 3, 4:
+    print(mess1[1]+mess2, count)
+else count == 5:
+    print(mess1[2]+mess2, count)
