5e764ffa9f723455b35bccff9af0589b1b31472e
Tommmmmmmmmmmmm <pol0026@nhs.vic.edu.au>
2018-05-09 14:00:13 +1000

    newsave

--- assessments/cat2_codeAndResearch/triviaGame.py
+++ assessments/cat2_codeAndResearch/triviaGame.py
@@ -8,8 +8,6 @@ Desc:   Maths Trivia Game
 qs = ['43+109=', '4*29=', '1836/12=', '259-74=', '3**5=']
 ans = ['152', '116', '153', '185', '243']
 
-print(qs[1])
-
 for i,item in enumerate(qs):
     inp=input(item)
     if inp == (ans[i]):
